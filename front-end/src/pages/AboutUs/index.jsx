b72fb475aa07fd874a0faa8a8decf41a688e9ac6
Ranbut <Ranbut_Beater@protonmail.com>
2024-10-22 12:34:39 -0300

    feat: added styled-components to AboutUs page

--- front-end/src/pages/AboutUs/index.jsx
+++ front-end/src/pages/AboutUs/index.jsx
@@ -1,16 +1,16 @@
+import styled from "styled-components";
 import Navbar from "../../components/Navbar";
 import Footer from "../../Footer";
-import "./style.css"
 import photo5 from "../../assets/photo-5.png"
 
 export default function AboutUs() {
     return(
         <>
             <Navbar/>
-                <div id="welcome" className="highlight">
-                <h2 id="title">Sobre nós</h2>
-                    <div className="cards">
-                        <div id="main-text">
+                <HistorySection>
+                    <HistoryTitle>Sobre nós</HistoryTitle>
+                    <HistoryContainer>
+                        <HistoryText>
                             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent
                             dapibus mollis ex quis faucibus. In condimentum, nisl a posuere feugiat,
                             nisl magna imperdiet odio, auctor feugiat metus sem sit amet lorem.  
@@ -20,26 +20,76 @@ export default function AboutUs() {
                             eu tortor. Proin vehicula ipsum eu lacinia interdum. Morbi faucibus  tortor odio, non 
                             vestibulum justo pretium eu. Suspendisse congue ipsum  sed mi finibus facilisis.
                             Vestibulum commodo posuere sagittis. Praesent tristique libero odio, nec malesuada urna tristique ac.
-                        </div>
+                        </HistoryText>
                         <img src={photo5} alt="Team" />
-                    </div>
-                </div>
-                <div id="why-section">
-                    <div className="why-section-box">
-                        <h4 id="why-title"><strong>Por quê escolher a Novo Horizonte</strong></h4>
-                        <div className="why-text">Etiam mauris eros, accumsan id diam  sed, consectetur dignissim orci. Donec faucibus magna lorem, non mattis 
+                    </HistoryContainer>
+                </HistorySection>
+                <WhySection>
+                    <WhyContainer>
+                        <WhyTitle><strong>Por quê escolher a Novo Horizonte</strong></WhyTitle>
+                        <WhyText>Etiam mauris eros, accumsan id diam  sed, consectetur dignissim orci. Donec faucibus magna lorem, non mattis 
                         justo malesuada sed. Vivamus faucibus nisl in mi aliquam dictum. 
                         Phasellus volutpat orci id risus dignissim accumsan. Phasellus fermentum dolor non erat fermentum fringilla. 
-                        Nulla ut mauris a lacus porttitor malesuada et ac justo.</div>
-                    </div>
-                    <div className="why-section-box">
-                        <div className="why-text">Duis ac pulvinar mauris. Aenean eget dolor eget velit imperdiet pharetra  nec in quam. Praesent congue commodo nisi, 
+                        Nulla ut mauris a lacus porttitor malesuada et ac justo.</WhyText>
+                    </WhyContainer>
+                    <WhyContainer>
+                        <WhyText>Duis ac pulvinar mauris. Aenean eget dolor eget velit imperdiet pharetra  nec in quam. Praesent congue commodo nisi, 
                         a aliquam enim ornare eget.  Aliquam convallis convallis libero sit amet scelerisque. 
-                        Morbi pellentesque vestibulum odio in condimentum.</div>
-                    <button>Veja os centros de saúde que temos.</button>
-                    </div>
-                </div>
+                        Morbi pellentesque vestibulum odio in condimentum.</WhyText>
+                        <button>Veja os centros de saúde que temos.</button>
+                    </WhyContainer>
+                </WhySection>
             <Footer/>
         </>
     )
 }
+
+const HistorySection = styled.div`
+    background: linear-gradient(180deg, rgb(19, 96, 132) 0%, rgb(12.27, 51.69, 70.13) 100%);
+    margin-top: 30px;
+    img {
+        width: 555px;
+        height: 275px;
+    }
+`;
+
+const HistoryTitle = styled.h1`
+    text-align: center;
+`;
+
+const HistoryContainer = styled.h2`
+    margin-top: 50px;
+    margin-left: 13vw;
+    display: flex;
+    gap: 130px;
+`;
+
+const HistoryText = styled.div`
+    color: white;
+    text-shadow: 0px 2px #000000;
+    font-size: 24px;
+    width: 671px;
+    height: 375px;
+`;
+
+const WhySection = styled.div`
+    text-align: center;
+    margin-left: 20vw;
+    margin-right: 20vw;
+    display: flex;
+`;
+
+const WhyContainer = styled.div`
+
+`;
+
+const WhyTitle = styled.div`
+    font-size: 40px;
+`;
+
+const WhyText = styled.div`
+    width: 591px;
+    height: 196px;
+    text-shadow: 0px 2px #0000003f;
+    font-size: 24px;
+`;
