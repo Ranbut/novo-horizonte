7d2c9cded3c331beaf5c675ded8ef19e4647b31f
Ranbut <Ranbut_Beater@protonmail.com>
2024-10-22 11:47:12 -0300

    feat: added styled-components to Footer component

--- front-end/src/Footer/index.jsx
+++ front-end/src/Footer/index.jsx
@@ -1,22 +1,49 @@
-import './style.css'
+import styled from 'styled-components'
 
 export default function Footer(){
     return(
-        <footer className='footer'>
-            <div className='clinic-cards'>
-                <div className='clinic-card'>
-                    <div className="clinic-title">Unidade I</div>
-                    <div className="clinic-text">Telefone: (XX) xxxxx-xxxx</div>
-                    <div className="clinic-text">Endereço: xxxxxxxxxxxxxxxxxxxx</div>
-                    <div className="clinic-text">Atendimento: das xxh às xxh, xxxxxx e xxxxxx</div>
-                </div>
-                <div className='clinic-card'>
-                    <div className="clinic-title">Unidade II</div>
-                    <div className="clinic-text">Telefone: (XX) xxxxx-xxxx</div>
-                    <div className="clinic-text">Endereço: xxxxxxxxxxxxxxxxxxxx</div>
-                    <div className="clinic-text">Atendimento: das xxh às xxh, xxxxxx e xxxxxx</div>
-                </div>
-            </div>
-        </footer>
+        <FooterBody>
+            <ClinicsContainer>
+                <ClinicContainer>
+                    <Title>Unidade I</Title>
+                    <Text>Telefone: (XX) xxxxx-xxxx</Text>
+                    <Text>Endereço: xxxxxxxxxxxxxxxxxxxx</Text>
+                    <Text>Atendimento: das xxh às xxh, xxxxxx e xxxxxx</Text>
+                </ClinicContainer>
+                <ClinicContainer>
+                    <Title>Unidade II</Title>
+                    <Text>Telefone: (XX) xxxxx-xxxx</Text>
+                    <Text>Endereço: xxxxxxxxxxxxxxxxxxxx</Text>
+                    <Text>Atendimento: das xxh às xxh, xxxxxx e xxxxxx</Text>
+                </ClinicContainer>
+            </ClinicsContainer>
+        </FooterBody>
     )
 }
+
+const FooterBody = styled.footer`
+    background: linear-gradient(180deg, rgb(19, 96, 132) 0%, rgb(12.27, 51.69, 70.13) 100%);
+    color: white;
+    text-shadow: 0px 2px #000000;
+`;
+
+const ClinicsContainer = styled.div`
+    margin-left: 20vw;
+    margin-right: 20vw;
+    display: flex;
+    gap: 10%;
+`;
+
+const ClinicContainer = styled.div`
+    margin-top: 67px;
+    margin-bottom: 68px;
+`;
+
+const Title = styled.div`
+    font-size: 40px;
+`;
+
+
+const Text = styled.div`
+    font-size: 24px;
+`;
